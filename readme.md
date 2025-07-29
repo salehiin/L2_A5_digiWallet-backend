440a0c998e0a866764f208d121c61d5426232dc0
salehiin <salehin.ay@gmail.com>
2025-07-29 17:45:11 +0600

    project set up done

---
+++ readme.md
@@ -0,0 +1,21 @@
+
+
+# 25.03
+
+auth=auth, user=user, tour=wallet, booking=transaction, division=commission, payment=payment, sslCommerz=sslCommerz, commission, admin
+tour management: , , , , , , 
+
+according to tour management find similiar data type in digital wallet
+
+UniWallet
+DigiWallet
+https://github.com/Apollo-Level2-Web-Dev/B5A5/blob/main/1.%20Digital%20Wallet%20System.md
+
+# 25.04 - data modelling
+# 25.05 - 
+# 25.06 - api end points - should check when do
+# 25.07 - project setup
+# 25.10 -
+# 25.11
+
+L2_A5_digiWallet-backend
---
+++ src/server.ts
@@ -0,0 +1,70 @@
+/* eslint-disable no-console */
+
+// import express, { Request, Response } from "express";
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+let server: Server;
+
+
+const startServer = async () => {
+    try {
+        // console.log(process.env.NODE_ENV);
+        // console.log(envVars.NODE_ENV);
+        await mongoose.connect(envVars.DB_URL)
+
+        console.log("Connected to DB!!!")
+
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening to port ${envVars.PORT}`)
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer()
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled rejection detected... Server shutting down...", error);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
+process.on("uncaughtExceptionMonitor", (error) => {
+    console.log("Uncaught Exception detected... Server shutting down...", error);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
+process.on("SIGTERM", () => {
+    console.log("Sigterm signal received... Server shutting down...");
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("Sigint signal received... Server shutting down...");
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+})
+
